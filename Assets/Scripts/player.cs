eb6ae09743f40f52c1bcc01289087b745eb84202
megajuliana <megajuliana143@gmail.com>
2025-01-22 08:36:53 +0700

    game edukasi

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D body;
+    SpriteRenderer sprite;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+  
+    // Update is called once per frame
+    void Update()  
+    {
+        if(Input.GetKey(KeyCode.RightArrow)){
+            body.velocity = new Vector2(speed,body.velocity.y); 
+            sprite.flipX = false;
+        }   
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            body.velocity = new Vector2(-speed,body.velocity.y);  
+            sprite.flipX = true;
+        }
+         if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)){
+            body.velocity = new Vector2(0,body.velocity.y); 
+        }    
+    } 
+}
+    
